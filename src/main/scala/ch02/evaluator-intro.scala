4779095b52ab2b6e605d40f2eba1f15cbfc925f9
Akira Ueda <ue@gol.com>
2011-11-28 05:39:31 +0900

    2.5.2 Experimenting with other recommenders

--- src/main/scala/ch02/evaluator-intro.scala
+++ src/main/scala/ch02/evaluator-intro.scala
@@ -9,6 +9,7 @@ import org.apache.mahout.cf.taste.eval.RecommenderBuilder
 import org.apache.mahout.cf.taste.impl.model.file._
 import org.apache.mahout.cf.taste.impl.neighborhood._
 import org.apache.mahout.cf.taste.impl.recommender._
+import org.apache.mahout.cf.taste.impl.recommender.slopeone._
 import org.apache.mahout.cf.taste.impl.similarity._
 import org.apache.mahout.cf.taste.model._
 import org.apache.mahout.cf.taste.neighborhood._
@@ -19,21 +20,25 @@ import org.apache.mahout.common.RandomUtils
 /** 2.3.2 Running RecommenderEvaluator */
 object EvaluatorIntro extends App {
 
+  lazy val userBased = new RecommenderBuilder {
+    def buildRecommender(model: DataModel) = {
+      val similarity = new PearsonCorrelationSimilarity(model)
+      val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
+      new GenericUserBasedRecommender(model, neighborhood, similarity)
+    }
+  } 
+
+  lazy val slopeOne = new RecommenderBuilder {
+    def buildRecommender(model: DataModel) = new SlopeOneRecommender(model)
+  }
+
   RandomUtils.useTestSeed()
   //val model = new FileDataModel(new File("../MIA/src/main/java/mia/recommender/ch02/intro.csv"))
   val model = new FileDataModel(new File("data/ml-100k/ua.base"))
   val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator
   //val evaluator = new RMSRecommenderEvaluator
 
-  val builder = new RecommenderBuilder {
-    def buildRecommender(model: DataModel) = {
-      val similarity = new PearsonCorrelationSimilarity(model)
-      val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
-      new GenericUserBasedRecommender(model, neighborhood, similarity)
-    }
-  }
-
-  val score = evaluator.evaluate(builder, null, model, 0.7, 1.0)
+  val score = evaluator.evaluate(slopeOne, null, model, 0.7, 1.0)
   println(score)
 }
 
--- src/main/scala/ch02/irevalulator-intro.scala
+++ src/main/scala/ch02/irevalulator-intro.scala
@@ -8,6 +8,7 @@ import org.apache.mahout.cf.taste.eval.RecommenderBuilder
 import org.apache.mahout.cf.taste.impl.model.file._
 import org.apache.mahout.cf.taste.impl.neighborhood._
 import org.apache.mahout.cf.taste.impl.recommender._
+import org.apache.mahout.cf.taste.impl.recommender.slopeone._
 import org.apache.mahout.cf.taste.impl.similarity._
 import org.apache.mahout.cf.taste.model._
 import org.apache.mahout.cf.taste.neighborhood._
@@ -18,12 +19,7 @@ import org.apache.mahout.common.RandomUtils
 /** 2.3.2 Running RecommenderEvaluator */
 object IREvaluatorIntro extends App {
 
-  RandomUtils.useTestSeed()
-  //val model = new FileDataModel(new File("../MIA/src/main/java/mia/recommender/ch02/intro.csv"))
-  val model = new FileDataModel(new File("data/ml-100k/ua.base"))
-  val evaluator = new GenericRecommenderIRStatsEvaluator
-
-  val builder = new RecommenderBuilder {
+  lazy val userBased = new RecommenderBuilder {
     def buildRecommender(model: DataModel) = {
       val similarity = new PearsonCorrelationSimilarity(model)
       val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
@@ -31,8 +27,17 @@ object IREvaluatorIntro extends App {
     }
   }
 
+  lazy val slopeOne = new RecommenderBuilder {
+    def buildRecommender(model: DataModel) = new SlopeOneRecommender(model)
+  }
+
+  RandomUtils.useTestSeed()
+  //val model = new FileDataModel(new File("../MIA/src/main/java/mia/recommender/ch02/intro.csv"))
+  val model = new FileDataModel(new File("data/ml-100k/ua.base"))
+  val evaluator = new GenericRecommenderIRStatsEvaluator
+
   val stats = evaluator.evaluate(
-    builder, null, model, null, 2,
+    slopeOne, null, model, null, 2,
     GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD,
     1.0)
   println(stats)
